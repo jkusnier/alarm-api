1536c2432b8409ce8d4bcbe48ecc6cc2100d719f
Jason Kusnier <jkusnier@gmail.com>
2014-09-07 19:44:42 -0400

    Added methods for selecting single user/device

--- routes/v1.js
+++ routes/v1.js
@@ -8,10 +8,24 @@ router.get('/devices', function(req, res) {
   });
 });
 
+router.get('/devices/:device_id', function(req, res) {
+  var db = req.db;
+  db.collection('devices').findOne({_id: req.params.device_id}, function(err, result) {
+    res.json(result);
+  });
+});
+
 router.get('/users', function(req, res) {
   var db = req.db;
   db.collection('users').find().toArray(function (err, items) {
     res.json(items);
+  });
+});
+
+router.get('/users/:user_id', function(req, res) {
+  var db = req.db;
+  db.collection('users').findOne({_id: req.params.user_id}, function(err, result) {
+    res.json(result);
   });
 });
 
